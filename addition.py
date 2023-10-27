de860e5816ca8494b982fbb0c4aff55bec8a9f8e
bitaniya nassir <bitaniyanassir10@gmail.com>
2023-10-27 15:57:56 +0300

    first commit

---
+++ addition.py
@@ -0,0 +1,13 @@
+
+def add(a, b):
+    total = a + b
+    return total
+
+
+a = int(input("please enter the first number "))
+b = int(input("please enter the second number "))
+"Return the sum of a and b"
+"*** YOUR CODE HERE ***"
+
+total = add(a, b)
+print(total)
